f7f948d4a5a3ce713057d30d3e20f93bbbc68304
saladinjake <juwavictor@gmail.com>
2022-12-03 21:00:00 +0100

    build: extend tailwind theme with financial color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        finance: {
+          dark: '#0f172a',
+          profit: '#10b981',
+          loss: '#ef4444',
+          accent: '#6366f1'
+        }
+      },
+      boxShadow: {
+        'finance': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      }
+    },
   },
   plugins: [],
 }
