b6b0050b2a95ef695cddf46909361c77c1db8eb9
saladinjake <juwavictor@gmail.com>
2022-12-04 03:00:00 +0100

    build: finalize tailwind config with typography and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
       },
       boxShadow: {
         'finance': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       }
     },
   },
