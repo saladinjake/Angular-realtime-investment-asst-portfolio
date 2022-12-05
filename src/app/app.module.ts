b00368b2c973dda4e0e1b7003ad28a893198df9f
saladinjake <juwavictor@gmail.com>
2022-12-05 03:00:00 +0100

    feat: register core modules and icons in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LucideAngularModule, LayoutDashboard, TrendingUp, TrendingDown, Wallet, Activity, PieChart, Plus, Trash2 } from 'lucide-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LucideAngularModule.pick({ LayoutDashboard, TrendingUp, TrendingDown, Wallet, Activity, PieChart, Plus, Trash2 })
   ],
   providers: [],
   bootstrap: [AppComponent]
